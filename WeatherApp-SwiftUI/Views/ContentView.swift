3cf6a04f4f5e527a5186374a334edf4bb523edf1
gembalisandesh <93411433+gembalisandesh@users.noreply.github.com>
2024-01-09 14:21:28 +0530

    Update ContentView.swift

--- WeatherApp-SwiftUI/Views/ContentView.swift
+++ WeatherApp-SwiftUI/Views/ContentView.swift
@@ -1,27 +1,31 @@
+//
+//  ContentView.swift
+//  WeatherApp-SwiftUI
+//
+//  Created by Gembali Sandesh Kumar on 07/01/24.
+//
+
 import SwiftUI
+import CoreLocation
 
 struct ContentView: View {
-    
     @ObservedObject var cityVM = WeatherViewModel()
+    @ObservedObject var locationManager = LocationManager.shared
     @State private var isLoading = true
 
     var body: some View {
-
         ZStack(alignment: .bottom) {
-            
             VStack(spacing: 0) {
-                
                 HeaderView(cityVM: cityVM)
                     .padding()
-                
+
                 ScrollView(showsIndicators: false) {
-                    
                     CityStatusView(cityVM: cityVM)
                 }
                 .padding(.top, 10)
             }
             .padding(.top, 40)
-            
+
             if isLoading {
                 LoadingView()
             }
@@ -29,11 +33,15 @@ struct ContentView: View {
         .background(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)), Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))]), startPoint: .topLeading, endPoint: .topTrailing))
         .edgesIgnoringSafeArea(.all)
         .onAppear {
-            // Simulate data loading (replace with your actual data fetching logic)
+            locationManager.requestAuthorization()
+            if locationManager.isAuthorized {
+                cityVM.getWeatherForCurrentLocation()
+            }
             DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
                 isLoading = false
             }
         }
+
     }
 }
 
